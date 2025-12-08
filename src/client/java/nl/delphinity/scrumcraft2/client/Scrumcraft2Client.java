3d833b2d4f3730909fc07cb63bd41a2fe6b12641
nullpointer <null.ptr.exc@outlook.com>
2025-12-08 17:48:14 +0100

    - Added project files from autogenerated project

---
+++ src/client/java/nl/delphinity/scrumcraft2/client/Scrumcraft2Client.java
@@ -0,0 +1,10 @@
+package nl.delphinity.scrumcraft2.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Scrumcraft2Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
