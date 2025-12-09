7bb6d914a513a1258691864a244eb265729bd7ce
Tim-olr <timolree1@gmail.com>
2025-12-09 19:55:18 +0100

    ball recipe

--- src/client/java/nl/delphinity/scrumcraft2/datagen/RecipeProvider.java
+++ src/client/java/nl/delphinity/scrumcraft2/datagen/RecipeProvider.java
@@ -3,7 +3,13 @@ package nl.delphinity.scrumcraft2.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import nl.delphinity.scrumcraft2.init.ModItems;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
@@ -26,7 +32,16 @@ public class RecipeProvider extends FabricRecipeProvider {
             @Override
             public void buildRecipes() {
                 // Recipes go here
-            }
+                HolderLookup.RegistryLookup<Item> itemLookup = registries.lookupOrThrow(Registries.ITEM);
+                // shapeless recipe for scrum ball
+                shapeless(RecipeCategory.COMBAT, ModItems.SCRUM_BALL, 1)
+                        .requires(Items.GOLD_INGOT)
+                        .requires(Items.LAPIS_LAZULI)
+                        .requires(Items.REDSTONE)
+                        .requires(Items.EMERALD)
+                        .unlockedBy(getHasName(Items.GOLD_INGOT), this.has(Items.GOLD_INGOT))
+                        .save(exporter, "scrum_ball");
+            };
         };
     }
 
