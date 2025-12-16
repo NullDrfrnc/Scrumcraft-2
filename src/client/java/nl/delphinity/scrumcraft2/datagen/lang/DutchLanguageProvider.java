24c15060f1b38113be175b6a107470c4e9286cef
OdeW2006 <odewelsing@gmail.com>
2025-12-16 21:05:24 +0100

    Evil snowman toevoeging (#19)
    * -EvilSnowman temp add, still needs texture fix
    
    * -finished snowman
    
    * Name fix

--- src/client/java/nl/delphinity/scrumcraft2/datagen/lang/DutchLanguageProvider.java
+++ src/client/java/nl/delphinity/scrumcraft2/datagen/lang/DutchLanguageProvider.java
@@ -2,6 +2,7 @@ package nl.delphinity.scrumcraft2.datagen.lang;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.core.HolderLookup;
+import nl.delphinity.scrumcraft2.init.ModEntityTypes;
 import nl.delphinity.scrumcraft2.init.ModBlocks;
 import nl.delphinity.scrumcraft2.init.ModItems;
 
@@ -29,6 +30,7 @@ public class DutchLanguageProvider extends AbstractLanguageProvider {
         translationBuilder.add(ModItems.AYRAN, "Heerlijke Zoute Kwark");
         translationBuilder.add(ModItems.POTION_OF_TERRORISM, "Brouwsel Van Terrorisme");
         translationBuilder.add(ModItems.AGARTHA_POTION, "Agartha Brouwsel");
+        translationBuilder.add(ModEntityTypes.EVIL_SNOW_GOLEM, "Slechtaardige sneeuwman");
         translationBuilder.add(ModBlocks.SCRUM_BLOCK, "Scrum Blok");
 
         // ItemGroups
